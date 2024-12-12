53030c35e0e272b7a44cdf464182093aaeee9aae
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-13 08:17:21 +0900

    feat(style): Tailwind 설정 추가
    - tailwind.config.js 생성
    - 커스텀 색상 설정 추가
    - 네비게이션 스타일 수정

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./src/**/*.{cljs,cljc}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        custom: '#F5BB1B',  // 노란색 계열의 강조색
+      }
+    }
+  },
+  plugins: []
+} 
