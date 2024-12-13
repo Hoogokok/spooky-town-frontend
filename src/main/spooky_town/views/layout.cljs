e05e9b451b323ae0d11a9c09385ac6ef744afeb0
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-13 17:40:35 +0900

    refactor(layout): 사용하지 않는 nav 삭제

--- src/main/spooky_town/views/layout.cljs
+++ src/main/spooky_town/views/layout.cljs
@@ -1,7 +1,6 @@
 (ns spooky-town.views.layout
   (:require [re-frame.core :as rf]
-            [spooky-town.views.layout.main :refer [main-layout]]
-            [spooky-town.views.layout.nav :refer [nav]]))
+            [spooky-town.views.layout.main :refer [main-layout]]))
 
 (defn root []
   (let [current-route @(rf/subscribe [:current-route])]
