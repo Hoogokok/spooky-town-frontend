2e49cc13a4c7e8ec1b84ba4dbdad41cbea31c5aa
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-13 09:08:47 +0900

    fix(core): React 18 createRoot API 적용
    - reagent.dom을 reagent.dom.client로 변경
    - createRoot 사용하도록 수정
    - 핫 리로드 지원 추가

--- src/main/spooky_town/core.cljs
+++ src/main/spooky_town/core.cljs
@@ -1,17 +1,19 @@
 (ns spooky-town.core
-  (:require [reagent.dom :as rdom]
+  (:require [reagent.dom.client :as rdom]
             [re-frame.core :as rf]
-            [spooky-town.events]
-            [spooky-town.subs]
             [spooky-town.routes :as routes]
-            [spooky-town.views.layout :as layout]))
+            [spooky-town.views.layout :as layout]
+            [spooky-town.events]
+            [spooky-town.subs]))
+
+(defonce root (rdom/create-root (.getElementById js/document "app")))
 
 (defn ^:dev/after-load reload! []
   (rf/clear-subscription-cache!)
-  (.log js/console "Code updated."))
+  (.log js/console "Code updated.")
+  (rdom/render root [layout/root]))
 
 (defn ^:export init []
   (rf/dispatch-sync [:initialize-db])
   (routes/init-routes!)
-  (rdom/render [layout/root]
-              (.getElementById js/document "app"))) 
+  (rdom/render root [layout/root])) 
