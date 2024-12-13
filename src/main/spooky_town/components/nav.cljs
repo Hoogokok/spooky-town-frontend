9756689fd5d666897a9c8040bbb320c24ec825a8
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-13 18:27:11 +0900

    fix: 모바일 메뉴 렌더링 문제 수정
    - 모바일 메뉴 z-index 값 조정 (z-50)
    - 메뉴 패널 위치 및 크기 설정 수정
    - 스크롤 가능한 오버플로우 처리 추가
    - 메뉴 컨텐츠 레이아웃 개선

--- src/main/spooky_town/components/nav.cljs
+++ src/main/spooky_town/components/nav.cljs
@@ -30,17 +30,17 @@
      [:button.inline-flex.items-center.justify-center.p-2.rounded-md.text-gray-400.hover:text-white.hover:bg-gray-700
       {:on-click #(rf/dispatch [:toggle-mobile-menu])}
       [:i.fas.fa-bars.h-6.w-6]]
-     
-     ;; 모바일 메뉴 패널
-     [:div.fixed.inset-0.z-40
+
+     ;; 모바일 메뉴 패널 (z-index 및 위치 조정)
+     [:div.fixed.inset-0.z-50
       {:class (if menu-open? "block" "hidden")}
-      
-      ;; 배경 오버레이
+
+      ;; 배경 오버레이 (z-index 조정)
       [:div.fixed.inset-0.bg-black.opacity-50
        {:on-click #(rf/dispatch [:toggle-mobile-menu])}]
-      
-      ;; 메뉴 컨텐츠
-      [:div.fixed.inset-y-0.right-0.max-w-xs.w-full.bg-gray-800.shadow-xl
+
+      ;; 메뉴 컨텐츠 (위치 및 스타일 조정)
+      [:div.fixed.top-0.right-0.h-full.w-64.bg-gray-800.shadow-xl.overflow-y-auto
        [:div.flex.items-center.justify-between.h-16.px-4.border-b.border-gray-700
         [:div.flex-shrink-0
          [:img.h-8.w-auto
@@ -49,7 +49,7 @@
         [:button.inline-flex.items-center.justify-center.p-2.rounded-md.text-gray-400.hover:text-white.hover:bg-gray-700
          {:on-click #(rf/dispatch [:toggle-mobile-menu])}
          [:i.fas.fa-times.h-6.w-6]]]
-       
+
        [:div.px-2.pt-2.pb-3.space-y-1
         [mobile-nav-item {:href "#" :label "홈" :active? true}]
         [mobile-nav-item {:href "#" :label "배포 채널"}]
